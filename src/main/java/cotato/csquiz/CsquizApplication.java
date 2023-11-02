ffc63a0edaae9509ccc38e1bf1c105243d7d6606
youth <boysoeng@naver.com>
2023-11-02 17:04:52 +0900

    initial commit

---
+++ src/main/java/cotato/csquiz/CsquizApplication.java
@@ -0,0 +1,13 @@
+package cotato.csquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cotato/csquiz/CsquizApplicationTests.java
@@ -0,0 +1,13 @@
+package cotato.csquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
