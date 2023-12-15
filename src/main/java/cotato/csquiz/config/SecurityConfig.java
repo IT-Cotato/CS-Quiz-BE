df6c61e2ddb6d0205e8d3024f071e974b04dd704
Youth <boysoeng@naver.com>
2023-12-15 15:15:22 +0900

    feat(auth): member api 권한 추가
    - general, member, admin, education

--- src/main/java/cotato/csquiz/config/SecurityConfig.java
+++ src/main/java/cotato/csquiz/config/SecurityConfig.java
@@ -38,10 +38,10 @@ public class SecurityConfig {
         http.csrf().disable()
                 .cors().disable()
                 .formLogin().disable()
-                .addFilter(new JwtAuthenticationFilter(authenticationManager,jwtUtil, refreshTokenRepository))
+                .addFilter(new JwtAuthenticationFilter(authenticationManager, jwtUtil, refreshTokenRepository))
                 .addFilterBefore(new JwtAuthorizationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/v1/api/member/**").hasRole("GENERAL")
+                        .requestMatchers("/v1/api/member/**").hasAnyRole("GENERAL", "MEMBER", "ADMIN", "EDUCATION")
                         .anyRequest().permitAll()
                 );
 
