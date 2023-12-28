345a8687bb807b6174a02e592825dece8e52d417
yunhacandy <yunhacandy@g.hongik.ac.kr>
2023-12-28 17:10:35 +0900

    feat: 가입 신청자 리스트 확인

---
+++ src/main/java/cotato/csquiz/service/AdminService.java
@@ -0,0 +1,39 @@
+package cotato.csquiz.service;
+
+import cotato.csquiz.domain.dto.auth.MemberInfoResponse;
+import cotato.csquiz.domain.entity.Member;
+import cotato.csquiz.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+
+    public List<MemberInfoResponse> getApplicantList() {
+        List<Member> applicantList = memberRepository.findAll();
+
+        return applicantList.stream()
+                .map(member -> MemberInfoResponse.builder()
+                        .name(member.getName())
+                        .backFourNumber(member.getPhoneNumber().substring(member.getPhoneNumber().length() - 4))
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private void logMemberInfo(MemberInfoResponse memberInfoResponse) {
+        log.info("이름 + 번호 4자리: {}({})", memberInfoResponse.getName(), memberInfoResponse.getBackFourNumber());
+    }
+
+    public MemberInfoResponse getMemberInfo(String email) {
+        return memberService.getMemberInfo(email);
+    }
+}
+
