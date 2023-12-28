319d11abacdaf1f740e94a066370fedfd34bd1da
Youth <boysoeng@naver.com>
2023-12-28 15:46:23 +0900

    refactor: 누락된 import문 추가

--- src/main/java/cotato/csquiz/controller/GenerationController.java
+++ src/main/java/cotato/csquiz/controller/GenerationController.java
@@ -9,7 +9,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/api/generation")
@@ -20,12 +25,12 @@ public class GenerationController {
     private final GenerationService generationService;
 
     @GetMapping("")
-    public ResponseEntity<?> getGenerations(){
+    public ResponseEntity<?> getGenerations() {
         return ResponseEntity.ok().body(generationService.getGenerations());
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> addGeneration(@RequestBody AddGenerationRequest request){
+    public ResponseEntity<?> addGeneration(@RequestBody AddGenerationRequest request) {
         long generationId = generationService.addGeneration(request);
         AddGenerationResponse response = AddGenerationResponse.builder()
                 .generationId(generationId).build();
@@ -39,7 +44,7 @@ public class GenerationController {
     }
 
     @PatchMapping("/period")
-    public ResponseEntity<?> changePeriod(@RequestBody ChangePeriodRequest request){
+    public ResponseEntity<?> changePeriod(@RequestBody ChangePeriodRequest request) {
         generationService.changePeriod(request);
         return ResponseEntity.ok().build();
     }
