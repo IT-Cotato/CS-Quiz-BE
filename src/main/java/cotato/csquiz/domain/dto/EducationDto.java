367bf7c22bce99c5b9125a16d934d69d308ee55e
yunhacandy <yunhacandy@g.hongik.ac.kr>
2024-01-23 16:14:36 +0900

    refactor: status 관련 코드 삭제

--- src/main/java/cotato/csquiz/domain/dto/EducationDto.java
+++ src/main/java/cotato/csquiz/domain/dto/EducationDto.java
@@ -1,7 +1,6 @@
 package cotato.csquiz.domain.dto;
 
 import cotato.csquiz.domain.entity.Education;
-import cotato.csquiz.domain.entity.EducationStatus;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,15 +10,12 @@ public class EducationDto {
     private Long educationId;
     private String subject;
     private int educationNum;
-    private EducationStatus status;
-
 
     public static EducationDto convertFromEducation(Education education) {
         return EducationDto.builder()
                 .educationId(education.getId())
                 .subject(education.getSubject())
                 .educationNum(education.getNumber())
-                .status(education.getStatus())
                 .build();
     }
 }
