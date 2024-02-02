ccfc4a51d5bc6628f65d807a5f9008aee17154d3
yunhacandy <yunhacandy@g.hongik.ac.kr>
2024-02-02 16:22:37 +0900

    fix: 현재 활동 부원 목록 반환 에러 수정

--- src/main/java/cotato/csquiz/repository/MemberRepository.java
+++ src/main/java/cotato/csquiz/repository/MemberRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByPhoneNumber(String phone);
-    Optional<Member> findByRole(MemberRole memberRole);
+    Optional<Member> findAllByRole(MemberRole memberRole);
 }
--- src/main/java/cotato/csquiz/service/AdminService.java
+++ src/main/java/cotato/csquiz/service/AdminService.java
@@ -73,7 +73,7 @@ public class AdminService {
     }
 
     public List<ActiveMemberInfoResponse> getCurrentActiveMembers() {
-        Optional<Member> activeMembers = memberRepository.findByRole(MemberRole.MEMBER);
+        Optional<Member> activeMembers = memberRepository.findAllByRole(MemberRole.MEMBER);
 
         return activeMembers.stream()
                 .map(member -> {
