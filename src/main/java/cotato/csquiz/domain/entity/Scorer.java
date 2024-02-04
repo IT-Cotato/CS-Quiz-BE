31cbe1932da93acbee5b7a1701069dc4175317e4
youth <boysoeng@naver.com>
2024-02-04 14:48:30 +0900

    feat(entity): 득점자 테이블 제약조건 추가
    - quiz_id기준으로 unique 제약조건 추가

--- src/main/java/cotato/csquiz/domain/entity/Scorer.java
+++ src/main/java/cotato/csquiz/domain/entity/Scorer.java
@@ -10,11 +10,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "quiz_id"))
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Scorer extends BaseTimeEntity {
