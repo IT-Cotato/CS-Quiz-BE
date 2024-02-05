e1d6aa318b62c4f7ae82b9e3b516879f7f4bc1a3
Youth <boysoeng@naver.com>
2024-02-05 23:46:19 +0900

    refactor: 서비스 코드 @Transactional 어노테이션 추가

--- src/main/java/cotato/csquiz/service/AdminService.java
+++ src/main/java/cotato/csquiz/service/AdminService.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AdminService {
     private final MemberRepository memberRepository;
