563a70a5a47011ce947444ac4e600ce870d04c21
Youth <109585620+Youthhing@users.noreply.github.com>
2024-02-08 23:15:52 +0900

    Fix: 문제 등록시 한쪽 타입이 없을때 NPE 발생 수정 (#27)
    * refactor: 검증 로직 로그 수정
    
    - info -> error
    
    * fix(quiz): 문제 추가 시 객관식 또는 주관식으로 통일된 형태일 때 발생하는 에러 수정
    
    - 기본으로 DTO에 Arraylist할당

--- src/main/java/cotato/csquiz/domain/dto/quiz/CreateQuizzesRequest.java
+++ src/main/java/cotato/csquiz/domain/dto/quiz/CreateQuizzesRequest.java
@@ -1,11 +1,12 @@
 package cotato.csquiz.domain.dto.quiz;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class CreateQuizzesRequest {
 
-    private List<MultipleChoiceQuizRequest> multiples;
-    private List<CreateShortQuizRequest> shortQuizzes;
+    private List<MultipleChoiceQuizRequest> multiples = new ArrayList<>();
+    private List<CreateShortQuizRequest> shortQuizzes = new ArrayList<>();
 }
--- src/main/java/cotato/csquiz/service/ValidateService.java
+++ src/main/java/cotato/csquiz/service/ValidateService.java
@@ -16,14 +16,14 @@ public class ValidateService {
 
     public void checkDuplicateEmail(String email) {
         if (memberRepository.findByEmail(email).isPresent()) {
-            log.info("[회원 가입 실패]: 중복된 이메일 " + email);
+            log.error("[회원 가입 실패]: 중복된 이메일 " + email);
             throw new AppException(ErrorCode.EMAIL_DUPLICATED);
         }
     }
 
     public void checkDuplicatePhoneNumber(String phone) {
         if (memberRepository.findByPhoneNumber(phone).isPresent()) {
-            log.info("[회원 가입 실패]: 존재하는 전화번호 " + phone);
+            log.error("[회원 가입 실패]: 존재하는 전화번호 " + phone);
             throw new AppException(ErrorCode.PHONE_NUMBER_DUPLICATED);
         }
     }
