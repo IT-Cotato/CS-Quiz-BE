51494c9a6364cb1b67c2fb4eca29ae927d7e3ff8
gikhoon <ghikhoon@naver.com>
2024-02-09 00:10:46 +0900

    fix: RefusedMember 테이블 fetch=LAZY 설정

--- src/main/java/cotato/csquiz/domain/entity/RefusedMember.java
+++ src/main/java/cotato/csquiz/domain/entity/RefusedMember.java
@@ -5,6 +5,7 @@ import static jakarta.persistence.FetchType.LAZY;
 import cotato.csquiz.global.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,7 +26,7 @@ public class RefusedMember extends BaseTimeEntity {
     @Column(name = "refused_id")
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
