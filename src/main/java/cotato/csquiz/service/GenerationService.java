af13770171c3a7b157a653bf9f0bfe5daec1329f
gikhoon <ghikhoon@naver.com>
2024-02-11 15:43:58 +0900

    refactor: generation 만들 때 날짜를 int에서 LocalDate로 변경

--- src/main/java/cotato/csquiz/service/GenerationService.java
+++ src/main/java/cotato/csquiz/service/GenerationService.java
@@ -24,16 +24,13 @@ public class GenerationService {
 
     private final GenerationRepository generationRepository;
 
-    //기수 추가
     public AddGenerationResponse addGeneration(AddGenerationRequest request) {
-        LocalDate startDate = LocalDate.of(request.getStartYear(), request.getStartMonth(), request.getStartDay());
-        LocalDate endDate = LocalDate.of(request.getEndYear(), request.getEndMonth(), request.getEndDay());
-        checkPeriodValid(startDate, endDate);
+        checkPeriodValid(request.getStartDate(), request.getEndDate());
         checkNumberValid(request.getGenerationNumber());
         Generation generation = Generation.builder()
                 .number(request.getGenerationNumber())
-                .startDate(startDate)
-                .endDate(endDate)
+                .startDate(request.getStartDate())
+                .endDate(request.getEndDate())
                 .build();
         Generation savedGeneration = generationRepository.save(generation);
         return AddGenerationResponse.builder()
@@ -41,9 +38,7 @@ public class GenerationService {
                 .build();
     }
 
-    //모집 변경
     public void changeRecruiting(ChangeRecruitingRequest request) {
-        //해당 멤버가 운영진인지 확인 TODO
         Generation generation = generationRepository.findById(request.getGenerationId()).orElseThrow(
                 () -> new AppException(ErrorCode.DATA_NOTFOUND));
         generation.changeRecruit(request.isStatement());
@@ -51,24 +46,19 @@ public class GenerationService {
     }
 
     public void changePeriod(ChangePeriodRequest request) {
-        LocalDate startDate = LocalDate.of(request.getStartYear(), request.getStartMonth(), request.getStartDay());
-        LocalDate endDate = LocalDate.of(request.getEndYear(), request.getEndMonth(), request.getEndDay());
-        checkPeriodValid(startDate, endDate);
+        checkPeriodValid(request.getStartDate(), request.getEndDate());
         Generation generation = generationRepository.findById(request.getGenerationId()).orElseThrow(
                 () -> new AppException(ErrorCode.DATA_NOTFOUND));
-        generation.changePeriod(startDate, endDate);
-        log.info("change date " + startDate + " " + endDate);
+        generation.changePeriod(request.getStartDate(), request.getEndDate());
+        log.info("change date " + request.getStartDate() + " " + request.getEndDate());
     }
 
-    //기수 목록 알려주기
     public List<GenerationInfoResponse> getGenerations() {
         List<Generation> generations = generationRepository.findAll();
         return generations.stream()
                 .map(GenerationInfoResponse::from)
                 .toList();
     }
-
-    //시작 날짜가 끝나는 날짜보다 뒤면 오류 처리
     private void checkPeriodValid(LocalDate startDate, LocalDate endDate) {
         if (endDate.isBefore(startDate)) {
             log.info("날짜 오류");
