cb27cbcdcd344450a9e8dd7be76412f140fc9f44
Youth <109585620+Youthhing@users.noreply.github.com>
2024-02-19 04:01:57 +0900

    Refactor: 이메일 인증 화면 수정 및 퀴즈 반환 start 값 설정 (#68)
    * refactor: 이메일 인증 화면 수정
    
    * fix: 웹소켓 요청 시 start null반환 에러 해결

--- src/main/java/cotato/csquiz/domain/constant/EmailConstants.java
+++ src/main/java/cotato/csquiz/domain/constant/EmailConstants.java
@@ -7,13 +7,15 @@ public class EmailConstants {
     public static final String SIGNUP_SUBJECT = "안녕하세요! 코테이토 회원 가입 인증 코드입니다.";
     public static final String PASSWORD_SUBJECT = "안녕하세요! 코테이토 비밀번호 찾기 인증 코드입니다.";
     public static final String MESSAGE_PREFIX = ""
-            + "<div style='margin:20px;'>"
-            + "<div style='margin:20px;'>"
-            + "<h1> 코테이토 인증 코드 입니다! </h1> <br>"
-            + "CODE : <strong>";
-
+            + "<div style=\"background-color: #f2f2f2; padding: 20px;\">"
+            + "<div style=\"background-color: #ffffff; padding: 20px;\">"
+            + "<h1 style=\"color: #336699; font-family: 'Arial', sans-serif; font-size: 24px;\">코테이토 인증 코드 입니다!</h1>"
+            + "<br>"
+            + "<p style=\"font-family: 'Arial', sans-serif; font-size: 16px;\">CODE: <strong>";
     public static final String MESSAGE_SUFFIX = ""
-            + "</strong>"
-            + "<h3> 10분안에 입력 부탁드립니다. 감사합니다! <h3>"
-            + "<br>";
+            + "</strong></p>"
+            + "<br>"
+            + "<h3 style=\"color: #336699; font-family: 'Arial', sans-serif; font-size: 18px;\">10분 안에 입력 부탁드립니다. 감사합니다!</h3>"
+            + "</div>"
+            + "</div>";
 }
