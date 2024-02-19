1142bc0708e8e49f1d5166f35481053a30bd6b0e
Youth <109585620+Youthhing@users.noreply.github.com>
2024-02-20 01:49:50 +0900

    Feature: 교육 우승자 테이블 추가 (#73)
    * feat(winner): 교육 우승자 테이블 추가
    
    * feat(kingMember): 결승 진출자 테이블 추가
    
    - member와 1:1
    - 교육과 * : 1
    
    * chore: jpa config 추가
    
    - @EnableJpaAuditing 어노테이션 분리

--- src/main/java/cotato/csquiz/CsquizApplication.java
+++ src/main/java/cotato/csquiz/CsquizApplication.java
@@ -2,11 +2,9 @@ package cotato.csquiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableScheduling
 public class CsquizApplication {
 
