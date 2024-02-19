f5a3fe2ee9081920d47ef09a6abb92d2c3e196b3
남기훈 <52378919+gikhoon@users.noreply.github.com>
2024-02-19 17:27:06 +0900

    Feature: CS문제 제어 관리시 교육 오픈 상태 확인 기능 구현 (#69)

--- src/main/java/cotato/csquiz/exception/ErrorCode.java
+++ src/main/java/cotato/csquiz/exception/ErrorCode.java
@@ -47,7 +47,8 @@ public enum ErrorCode {
     QUIZ_TYPE_NOT_MATCH(HttpStatus.BAD_REQUEST, "주관식 정답만 추가 가능합니다."),
     CONTENT_IS_NOT_ANSWER(HttpStatus.BAD_REQUEST, "해당 문구는 정답이 아닙니다"),
     CONTENT_IS_ALREADY_ANSWER(HttpStatus.BAD_REQUEST, "이미 정답인 답을 추가했습니다"),
-    ENUM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 Enum을 resolve할 수 없습니다.");
+    ENUM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 Enum을 resolve할 수 없습니다."),
+    EDUCATION_CLOSED(HttpStatus.BAD_REQUEST, "CS 퀴즈가 닫혀 있습니다 먼저 교육 시작 버튼을 눌러주세요");
 
     private final HttpStatus httpStatus;
     private final String message;
