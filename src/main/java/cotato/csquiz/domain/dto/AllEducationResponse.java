64567cbd638b4973df077c1ff5aa734c6fd67f4c
yunhacandy <yunhacandy@g.hongik.ac.kr>
2024-02-20 18:30:25 +0900

    feat: AllEducationResponse에 세션 주차 추가

--- src/main/java/cotato/csquiz/domain/dto/AllEducationResponse.java
+++ src/main/java/cotato/csquiz/domain/dto/AllEducationResponse.java
@@ -10,12 +10,14 @@ public class AllEducationResponse {
     private Long educationId;
     private String subject;
     private int educationNumber;
+    private int sessionNumber;
 
     public static AllEducationResponse convertFromEducation(Education education) {
         return AllEducationResponse.builder()
                 .educationId(education.getId())
                 .subject(education.getSubject())
                 .educationNumber(education.getNumber())
+                .sessionNumber(education.getSession().getNumber())
                 .build();
     }
 }
