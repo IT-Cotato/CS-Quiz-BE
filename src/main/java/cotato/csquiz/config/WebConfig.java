5c80a76c1efecc53576cdd801967ad6959dabdba
youth <boysoeng@naver.com>
2024-02-22 00:18:26 +0900

    feat: access-control-allow-origin-header 추가

--- src/main/java/cotato/csquiz/config/WebConfig.java
+++ src/main/java/cotato/csquiz/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://www.cotato.kr:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
