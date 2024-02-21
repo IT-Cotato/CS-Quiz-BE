80f55b3442ae8b4ca311626200f6b3fcc1d053f7
youth <boysoeng@naver.com>
2024-02-21 17:31:17 +0900

    refactor: 부원 역할 반환 시 멤버 역할 반환 추가
    - MemberRole 추가
    - 메서드명 find형식으로 변경

--- src/main/java/cotato/csquiz/domain/dto/member/MemberEnrollInfoResponse.java
+++ src/main/java/cotato/csquiz/domain/dto/member/MemberEnrollInfoResponse.java
@@ -2,6 +2,7 @@ package cotato.csquiz.domain.dto.member;
 
 import cotato.csquiz.domain.entity.Member;
 import cotato.csquiz.domain.enums.MemberPosition;
+import cotato.csquiz.domain.enums.MemberRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,13 +18,15 @@ public class MemberEnrollInfoResponse {
     private String memberName;
     private MemberPosition position;
     private int generationNumber;
+    private MemberRole role;
 
     public static MemberEnrollInfoResponse from(Member member) {
         return new MemberEnrollInfoResponse(
                 member.getId(),
                 member.getName(),
                 member.getPosition(),
-                member.getGeneration().getNumber()
+                member.getGeneration().getNumber(),
+                member.getRole()
         );
     }
 }
