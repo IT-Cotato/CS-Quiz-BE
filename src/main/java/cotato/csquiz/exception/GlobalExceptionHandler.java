2ced404b5bf95bb6010c209b8a3ecc14fe7b991b
Youth <109585620+Youthhing@users.noreply.github.com>
2024-02-22 19:13:37 +0900

    Feat: 테이블 제약 조건 추가 및 Sql 예외 처리 (#100)
    * fix: 고아 객체 삭제 문제해결
    
    * feat: SQL 문제 관련 예외 처리

--- src/main/java/cotato/csquiz/exception/GlobalExceptionHandler.java
+++ src/main/java/cotato/csquiz/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package cotato.csquiz.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -31,5 +33,18 @@ public class GlobalExceptionHandler {
         log.error("요청 uri: {}", request.getRequestURI());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
                 .body(e.getErrorCode() + " " + e.getErrorCode().getMessage());
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> sqlException(SQLException e, HttpServletRequest request) {
+        log.error("발생한 에러: {}", e.getErrorCode());
+        log.error("에러 요청 uri: {}", request.getRequestURI());
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "SQL Error",
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(500).body(errorResponse);
     }
 }
