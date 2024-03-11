c051313e6631f4b0a5560558bf2b81187d8adef3
gikhoon <ghikhoon@naver.com>
2024-03-12 00:43:08 +0900

    refactor: Quiz 엔티티 mapping 설정 변경
    - quiz랑 연결하는 Record, Scorer를 cascade = CascadeType.REFRESH에서 orphanRemoval = true로 변경

--- src/main/java/cotato/csquiz/domain/entity/Quiz.java
+++ src/main/java/cotato/csquiz/domain/entity/Quiz.java
@@ -72,11 +72,11 @@ public class Quiz extends BaseTimeEntity {
     @JoinColumn(name = "generation_id")
     private Generation generation;
 
-    @OneToMany(mappedBy = "quiz", cascade = CascadeType.REFRESH)
+    @OneToMany(mappedBy = "quiz", orphanRemoval = true)
     @JsonIgnore
     private List<Record> records = new ArrayList<>();
 
-    @OneToOne(mappedBy = "quiz", cascade = CascadeType.REFRESH)
+    @OneToOne(mappedBy = "quiz", orphanRemoval = true)
     @JsonIgnore
     private Scorer scorer;
 
