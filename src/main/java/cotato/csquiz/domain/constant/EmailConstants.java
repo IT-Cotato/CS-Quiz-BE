5e1eda64cda4b8e7c6b7212d48ef40938b4ae463
Youth <boysoeng@naver.com>
2024-03-21 14:16:01 +0900

    feat: 이메일 인증용 계정 공식 계정으로 변경
    - 개인 -> itcotato

--- src/main/java/cotato/csquiz/domain/constant/EmailConstants.java
+++ src/main/java/cotato/csquiz/domain/constant/EmailConstants.java
@@ -2,7 +2,7 @@ package cotato.csquiz.domain.constant;
 
 public class EmailConstants {
 
-    public static final String SENDER_EMAIL = "boysoeng@g.hongik.ac.kr";
+    public static final String SENDER_EMAIL = "itcotato@gmail.com";
     public static final String SENDER_PERSONAL = "IT연합동아리 코테이토";
     public static final String SIGNUP_SUBJECT = "안녕하세요! 코테이토 회원 가입 인증 코드입니다.";
     public static final String PASSWORD_SUBJECT = "안녕하세요! 코테이토 비밀번호 찾기 인증 코드입니다.";
